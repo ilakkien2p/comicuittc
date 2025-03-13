503d8661e02fede7f0c237a4b9e16d8b08526a9c
ilakkien2p <ilakkienselvakumar@gmail.com>
2025-03-13 20:38:01 +0000

    Add files via upload

---
+++ comic/page-controls.js
@@ -0,0 +1,14 @@
+AFRAME.registerComponent("page-controls",{
+    schema:{
+        pages:{
+            type:"array",default:[
+                {page:"1",color:"black"},
+                {page:"2",color:"black"},
+                {page:"3",color:"black"},
+                {page:"4",color:"black"},
+                {page:"5",color:"black"},
+
+            ]
+        }
+    }
+})
